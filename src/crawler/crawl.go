c2de1bac9f0b75a4b654c7e4f82756836f66b3da
Ray Bejjani <ray@cloudflare.com>
2013-04-11 12:58:11 -0700

    Crawler coordinates via goroutines

--- src/crawler/crawl.go
+++ src/crawler/crawl.go
@@ -1,5 +1,6 @@
 /* A simple parallel grabber
- * Note: It has scheduling flaws
+ * It uses channels to ensure coordination between the master goroutine and
+ * url grabbers
  */
 package main
 
@@ -7,26 +8,35 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+	"time"
 )
 
-func getUrl(url string) {
+func getUrl(url string, duration chan time.Duration) {
 	fmt.Printf("Get %s\n", url)
+
+	start := time.Now()
+	defer func() { duration <- time.Since(start) }()
 
 	if resp, err := http.Get("http://example.com/"); err != nil {
 		fmt.Printf("Error fetching %s: %s\n", url, err)
 
 	} else {
-		fmt.Printf("Done with %s: %d bytes\n", url, resp.ContentLength)
+		fmt.Printf("Done with %s: %d bytes and status: %s\n", url, resp.ContentLength, resp.Status)
 	}
 }
 
 func main() {
 	urls := os.Args[1:]
+	var duration = make(chan time.Duration)
 
 	fmt.Printf("Getting %d urls: %v\n", len(urls), urls)
 
 	for _, url := range urls {
-		go getUrl(url)
+		go getUrl(url, duration)
+	}
+
+	for i := 0; i < len(urls); i++ {
+		<-duration
 	}
 
 	fmt.Printf("Done\n")

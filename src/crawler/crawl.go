a4267256a6f1fc72761327578f18ae66d7da41bf
Ray Bejjani <ray@cloudflare.com>
2013-04-11 15:47:45 -0700

    Added tracking for Vary headers using interfaces and type inference

--- src/crawler/crawl.go
+++ src/crawler/crawl.go
@@ -1,6 +1,7 @@
 /* A simple parallel grabber
  * It uses channels to ensure coordination between the master goroutine and
- * url grabbers
+ * url grabbers.
+ * It tracks durations and whether the responses included Vary headers.
  */
 package main
 
@@ -11,7 +12,20 @@ import (
 	"time"
 )
 
-func getUrl(url string, duration chan time.Duration) {
+type VarySender interface {
+	ICanHazVary() bool
+}
+
+type concreteVarySender struct {
+	Url     string
+	hazVary bool
+}
+
+func (ccs *concreteVarySender) ICanHazVary() bool {
+	return ccs.hazVary
+}
+
+func getUrl(url string, duration chan time.Duration, varies chan VarySender) {
 	fmt.Printf("Get %s\n", url)
 
 	start := time.Now()
@@ -21,22 +35,44 @@ func getUrl(url string, duration chan time.Duration) {
 		fmt.Printf("Error fetching %s: %s\n", url, err)
 
 	} else {
+		if resp.Header.Get(http.CanonicalHeaderKey("Vary")) != "" {
+			varies <- &concreteVarySender{
+				Url:     url,
+				hazVary: true}
+		}
+
 		fmt.Printf("Done with %s: %d bytes and status: %s\n", url, resp.ContentLength, resp.Status)
 	}
 }
 
 func main() {
-	urls := os.Args[1:]
-	var duration = make(chan time.Duration)
+	var (
+		urls     = os.Args[1:]
+		duration = make(chan time.Duration)
+		varies   = make(chan VarySender, 128)
+	)
 
 	fmt.Printf("Getting %d urls: %v\n", len(urls), urls)
 
 	for _, url := range urls {
-		go getUrl(url, duration)
+		go getUrl(url, duration, varies)
 	}
 
 	for i := 0; i < len(urls); i++ {
 		<-duration
+	}
+
+	close(varies)
+
+	for vary := range varies {
+		if vary.ICanHazVary() {
+			if ccs, ok := vary.(*concreteVarySender); ok {
+				fmt.Printf("%s hazes the varies\n", ccs.Url)
+
+			} else {
+				fmt.Printf("something hazes the varies\n")
+			}
+		}
 	}
 
 	fmt.Printf("Done\n")
